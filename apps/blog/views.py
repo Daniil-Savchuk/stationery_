0e3eb50d1d6c21e09e509d2fb4ca2a737ce0a92c
Daniil-Savchuk <danya.danisimo@gmail.com>
2022-12-09 16:20:52 +0200

    Blog part 2 Homework(pt.3)

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -21,5 +21,5 @@ def article_view(request, category_id, article_id):
 
 def tag_view(request, tag_id):
     tag = Tag.objects.get(id=tag_id)
-    tag_articles = tag.Article__set.all()
+    tag_articles = Article.objects.filter(tag=tag)
     return render(request, 'blog/tag_view.html', {'tag': tag, 'tag_articles': tag_articles})
