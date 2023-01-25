2aec0cda64e8ea22697d2fc712930bf66ee2e6ff
Daniil-Savchuk <danya.danisimo@gmail.com>
2023-01-25 12:43:58 +0200

    cart_hw

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.order.views import add_to_cart
+from apps.order.views import add_to_cart, cart_page
 
 
 urlpatterns = [
     path('add-to-cart/', add_to_cart, name='add_to_cart'),
+    path('cart-page/', cart_page, name='cart_page'),
 ]
